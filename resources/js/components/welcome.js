1c3b2999fff8b7f1b98f120f51a74f4e1a48ed1b
Oralita <oat431@gmail.com>
2020-11-20 19:46:12 +0700

    create the full react and axios

--- resources/js/components/welcome.js
+++ resources/js/components/welcome.js
@@ -1,23 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function Welcome(){
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className='card'>
-                        <div className='card-header'>The planning system</div>
-                        <div className='card-body'><a href='/course'>all the course in the system</a></div>
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+class Welcome extends Component {
+    constructor() {
+        super();
+        this.state = {
+            student: []
+        };
+    }
+    componentDidMount() {
+        axios.get("/api/student").then(res => {
+            this.setState({
+                student: res.data
+            });
+        });
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">
+                                The planning system
+                            </div>
+                            <div className="card-body">
+                                <a href="/course">
+                                    all the course in the system
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">
+                                The student In the list
+                            </div>
+                            <div className="card-body">
+                                <table className="table table-strip">
+                                    <thead>
+                                        <tr>
+                                            <th>Student ID</th>
+                                            <td>Student Name</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.student.map(s => (
+                                            <tr>
+                                                <td key={s.student_id}>{s.student_id}</td>
+                                                <td>{s.student_name}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default Welcome;
 
-if (document.getElementById('welcome')) {
-    ReactDOM.render(<Welcome />, document.getElementById('welcome'));
-}
+ReactDOM.render(<Welcome />, document.getElementById("welcome"));
